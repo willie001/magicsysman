e73ab06a4c08d0a163aa15c52a9fc3dafa56bd89
Eben Venter <eben.venter@gmail.com>
2017-08-06 18:19:55 +0800

    test1

--- MagicMaids/Controllers/SettingsController.cs
+++ MagicMaids/Controllers/SettingsController.cs
@@ -138,8 +138,13 @@ namespace MagicMaids.Controllers
                 ModelState.AddModelError(string.Empty, "Valid setting not found.");
             }
 
+			LogHelper log2 = new LogHelper(LogManager.GetCurrentClassLogger());
+			log2.Log(LogLevel.Debug, "<XXXXXX> 1" , nameof(SaveSettings), null, null);
+
             if (ModelState.IsValid)
 			{
+				log2.Log(LogLevel.Debug, "<XXXXXX> 2", nameof(SaveSettings), null, null);
+
 				Guid _id = setting.Id;
                 // get original rowversion before updating model
                 var rowVersion = setting.RowVersion;
@@ -150,9 +155,11 @@ namespace MagicMaids.Controllers
 					ModelState.AddModelError(string.Empty, $"Setting [{_id.ToString()}] not found.  Please try again.");
 					return JsonFormResponse();
 				}
+				log2.Log(LogLevel.Debug, "<XXXXXX> 3", nameof(SaveSettings), null, null);
                 
 				if (TryUpdateModel<SystemSetting>(_objToUpdate))
 				{
+					log2.Log(LogLevel.Debug, "<XXXXXX> 4", nameof(SaveSettings), null, null);
 					//https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/crud
 					_objToUpdate.UpdatedAt = DateTime.Now;
 					_objToUpdate.UpdatedBy = HttpContext.User.Identity.Name;
@@ -164,6 +171,7 @@ namespace MagicMaids.Controllers
 						MMContext.Entry(_objToUpdate).OriginalValues["RowVersion"] = rowVersion;
 						MMContext.SaveChanges();
 
+						log2.Log(LogLevel.Debug, "<XXXXXX> 5", nameof(SaveSettings), null, null);
 						return JsonSuccessResponse("Setting saved successfully");
 					}
                     catch (DbUpdateConcurrencyException ex)
@@ -216,9 +224,10 @@ namespace MagicMaids.Controllers
 
 			if (!ModelState.IsValid)
 			{
+				log2.Log(LogLevel.Debug, "<XXXXXX> 7", nameof(SaveSettings), null, null);
 				Helpers.LogFormValidationErrors(LogManager.GetCurrentClassLogger(), ModelState, nameof(SaveSettings), setting);
 			}
-
+			log2.Log(LogLevel.Debug, "<XXXXXX> 8", nameof(SaveSettings), null, null);
             return JsonFormResponse();
         }
 		#endregion
