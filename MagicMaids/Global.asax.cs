ff374a840d33044200d4b8c5555a0221e8b2a8c0
Eben Venter <eben.venter@gmail.com>
2018-01-22 22:02:42 +0800

    Test deploy

--- MagicMaids/Global.asax.cs
+++ MagicMaids/Global.asax.cs
@@ -102,7 +102,7 @@ namespace MagicMaids
 
 
 			// Manage to display a friendly view 
-			InvokeErrorAction(httpContext, exception, currentController, currentAction);
+			//InvokeErrorAction(httpContext, exception, currentController, currentAction);
 		}
 
 		protected void InvokeErrorAction(HttpContext context, Exception ex, String currentController, String currentAction)
