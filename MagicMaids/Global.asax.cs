4c250ea4e563163aa47753b034b38f04ca1d8f22
Eben Venter <eben.venter@gmail.com>
2018-01-23 23:28:13 +0800

    try

--- MagicMaids/Global.asax.cs
+++ MagicMaids/Global.asax.cs
@@ -49,61 +49,61 @@ namespace MagicMaids
 
         }
 
-		protected void Application_Error(object sender, EventArgs e)
-		{
-			Exception exception = Server.GetLastError();
+		//protected void Application_Error(object sender, EventArgs e)
+		//{
+		//	Exception exception = Server.GetLastError();
 
-			// Clear the response stream 
-			var httpContext = ((HttpApplication)sender).Context;
-			var currentController = String.Empty;
-			var currentAction = String.Empty;
-			var currentRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
+		//	// Clear the response stream 
+		//	var httpContext = ((HttpApplication)sender).Context;
+		//	var currentController = String.Empty;
+		//	var currentAction = String.Empty;
+		//	var currentRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
 
-			if (currentRouteData != null)
-			{
-				if (currentRouteData.Values["controller"] != null && !String.IsNullOrEmpty(currentRouteData.Values["controller"].ToString()))
-				{
-					currentController = currentRouteData.Values["controller"].ToString();
-				}
+		//	if (currentRouteData != null)
+		//	{
+		//		if (currentRouteData.Values["controller"] != null && !String.IsNullOrEmpty(currentRouteData.Values["controller"].ToString()))
+		//		{
+		//			currentController = currentRouteData.Values["controller"].ToString();
+		//		}
 
-				if (currentRouteData.Values["action"] != null && !String.IsNullOrEmpty(currentRouteData.Values["action"].ToString()))
-				{
-					currentAction = currentRouteData.Values["action"].ToString();
-				}
-			}
+		//		if (currentRouteData.Values["action"] != null && !String.IsNullOrEmpty(currentRouteData.Values["action"].ToString()))
+		//		{
+		//			currentAction = currentRouteData.Values["action"].ToString();
+		//		}
+		//	}
 
-			httpContext.ClearError();
-			httpContext.Response.Clear();
-			httpContext.Response.StatusCode = exception is HttpException ? ((HttpException)exception).GetHttpCode() : 500;
-			httpContext.Response.TrySkipIisCustomErrors = true;
+		//	httpContext.ClearError();
+		//	httpContext.Response.Clear();
+		//	httpContext.Response.StatusCode = exception is HttpException ? ((HttpException)exception).GetHttpCode() : 500;
+		//	httpContext.Response.TrySkipIisCustomErrors = true;
 
-			if (exception != null)
-			{
-				// bypass this error for now to avoid logging.
-				if (exception.Source != null && (exception.Source.ToString() == "System.Web" && exception.Message == "This type of page is not served."))
-					return;
+		//	if (exception != null)
+		//	{
+		//		// bypass this error for now to avoid logging.
+		//		if (exception.Source != null && (exception.Source.ToString() == "System.Web" && exception.Message == "This type of page is not served."))
+		//			return;
 
-				//if (exception.InnerException != null && exception.InnerException is System.Web.Compilation.CompilationExcetion)
-				//{
-				//	HttpCompileException ex = (HttpCompileException)exception.InnerException;
+		//		//if (exception.InnerException != null && exception.InnerException is System.Web.Compilation.CompilationExcetion)
+		//		//{
+		//		//	HttpCompileException ex = (HttpCompileException)exception.InnerException;
 
-				//}
-				var logger = LogManager.GetCurrentClassLogger();
-				if (!String.IsNullOrWhiteSpace(currentController) )
-				{
-					logger.Log(LogLevel.Error, exception, $"Exception from {currentController}.{currentAction}");
-				}
-				else
-				{
-					logger.Log(LogLevel.Error, exception, $"Unhandled Exception");
-				}
+		//		//}
+		//		var logger = LogManager.GetCurrentClassLogger();
+		//		if (!String.IsNullOrWhiteSpace(currentController) )
+		//		{
+		//			logger.Log(LogLevel.Error, exception, $"Exception from {currentController}.{currentAction}");
+		//		}
+		//		else
+		//		{
+		//			logger.Log(LogLevel.Error, exception, $"Unhandled Exception");
+		//		}
 
-			}
+		//	}
 
 
-			// Manage to display a friendly view 
-			//InvokeErrorAction(httpContext, exception, currentController, currentAction);
-		}
+		//	// Manage to display a friendly view 
+		//	InvokeErrorAction(httpContext, exception, currentController, currentAction);
+		//}
 
 		protected void InvokeErrorAction(HttpContext context, Exception ex, String currentController, String currentAction)
 		{
