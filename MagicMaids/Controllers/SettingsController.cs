88bfc3ac7b5e04d505ec31705caca479a86f7108
Eben Venter <eben.venter@gmail.com>
2018-03-21 23:47:05 +0800

    ff

--- MagicMaids/Controllers/SettingsController.cs
+++ MagicMaids/Controllers/SettingsController.cs
@@ -48,7 +48,7 @@ namespace MagicMaids.Controllers
 				Formatting = Formatting.Indented
 			};
 
-			var connstring = "Server=mi3-wts5.a2hosting.com;Port=3306;Uid=magic_maids;Pwd=wC51ur51;Database=magicdry_db;Pooling=False;sslmode=None;";
+			var connstring = "SERVER=mi3-wts5.a2hosting.com;PORT=3306;UID=magic_maids;PWD=wC51ur51;DATABASE=magicdry_db;POOLING=False;SSLMODE=None;";
 			Stopwatch stopwatch= new Stopwatch();
 			MySqlConnection connection = null;
 			try
