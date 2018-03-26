bbcb327ab2de05f5ac955c83789b8786a24607b8
Eben Venter <eben.venter@gmail.com>
2018-03-26 21:17:30 +0800

    test

--- MagicMaids/Helpers/Extensions.cs
+++ MagicMaids/Helpers/Extensions.cs
@@ -48,28 +48,17 @@ namespace MagicMaids
 
 		private static DateTime GetClientDateTime(DateTime dt)
 		{
-			LogHelper log = new LogHelper(LogManager.GetCurrentClassLogger());
-			log.Log(LogLevel.Info, "GetClientDateTime", nameof(ToClientDateTime), null, null);
 			var timeOffSet = HttpContext.Current.Session["timezoneoffset"];  // read the value from session
-			var debug = $"Input: {dt.ToString()}";
 
 			if (timeOffSet != null)
 			{
-				debug += $" | Offset: {timeOffSet.ToString()}";
-
 				var offset = int.Parse(timeOffSet.ToString());
 				dt = dt.AddMinutes(-1 * offset);
-				debug += $" | Output: {dt.ToString()}";
-
 
 				return dt;
 			}
 
 			// if there is no offset in session return the datetime in server timezone
-			debug += $" | Local Output: {dt.ToLocalTime().ToString()}";
-
-			log.Log(LogLevel.Info, "Date Time Convert:", nameof(ToClientDateTime), null, debug);
-
 			return dt.ToLocalTime();
 		}
 	}
