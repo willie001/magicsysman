4d4be9776352b62bd85e682a713037ad0087a3b2
Eben Venter <eben.venter@gmail.com>
2018-06-10 10:59:45 +0800

    date error on neutral date

--- MagicMaids/Scripts/app/app.module.js
+++ MagicMaids/Scripts/app/app.module.js
@@ -141,15 +141,19 @@
 		      priority: 1,
 		      require: 'ngModel',
 		      link: function (scope, element, attrs, ctrl) {
-		        ctrl.$formatters.push(function (value) {
-		          var date = new Date(Date.parse(value));
-		          date = new Date(date.getTime() + (60000 * date.getTimezoneOffset()));
-		          return date;
+				    ctrl.$formatters.push(function (value) {
+						if (value == undefined)
+						{
+							return null;
+						}
+						var date = new Date(Date.parse(value));
+	          			date = new Date(date.getTime() + (60000 * date.getTimezoneOffset()));
+	          			return date;
 		        });
 
 		        ctrl.$parsers.push(function (value) {
-		          var date = new Date(value.getTime() - (60000 * value.getTimezoneOffset()));
-		          return date;
+						var date = new Date(value.getTime() - (60000 * value.getTimezoneOffset()));
+		          		return date;
 		        });
 		      }
 		  }
