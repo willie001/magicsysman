0428fded9785db988dbbaf83c42eb3a341f1632e
Eben Venter <eben.venter@gmail.com>
2018-06-13 17:38:20 +0800

    d

--- MagicMaids/Helpers/DateTimeWrapper.cs
+++ MagicMaids/Helpers/DateTimeWrapper.cs
@@ -124,12 +124,12 @@ namespace MagicMaids
 				LogHelper.LogDebugDetails("CleanersController.SaveCleanerRoster - Local", _dt.ToString());
 
 				//_dt = DateTime.SpecifyKind(_dt, DateTimeKind.Unspecified);
-				_dt = _dt.ToUTC();
+				var _dt1 = _dt.ToUTC();
 
-				LogHelper.LogDebugDetails("CleanersController.SaveCleanerRoster - UTC", _dt.ToString());
+				LogHelper.LogDebugDetails("CleanersController.SaveCleanerRoster - UTC", _dt1.ToString());
 				// TimePicker changes new times to Local Kind but it is UTC.
-				_dt = _dt.ToUser();
-				LogHelper.LogDebugDetails("CleanersController.SaveCleanerRoster - User", _dt.ToString());
+				_dt1 = _dt1.ToUser();
+				LogHelper.LogDebugDetails("CleanersController.SaveCleanerRoster - User", _dt1.ToString());
 			}
 
 			var _hr = _dt.Hour;
